95a0963b3302f9a523247ac4f3984c06455e7828
Anthony Truong <quocanh1104@gmail.com>
2023-10-21 04:30:15 +1030

    Install tailwind

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { styled } from 'nativewind';
+import { List } from './components/List';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='pt-12 items-center'>
+      <List />
     </View>
   );
 }
